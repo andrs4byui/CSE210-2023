44dbc48d6e915b663796fe156bde888af4d8d5cf
Andres Soruco <mam20002@byui.edu>
2023-01-07 22:10:02 -0400

    Prep 1 and 2 Completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        String userGrade = Console.ReadLine();
+
+        int userGradeNumber = int.Parse(userGrade);
+
+
+        if (userGradeNumber >= 90)
+            {
+                Console.WriteLine("You got an A");
+            }
+        else if (userGradeNumber >= 80)
+            {
+                Console.WriteLine("You got an B");
+            }
+        else if (userGradeNumber >= 70)
+            {
+                Console.WriteLine("You got an C");
+            }
+        else if (userGradeNumber >= 60)
+            {
+                Console.WriteLine("You got an D");
+            }
+        else
+            {
+                Console.WriteLine("You got an F");
+            };    
+        if (userGradeNumber > 70)
+            {
+                Console.WriteLine("Congratulations!! You Have passed the course.");
+            }
+        else 
+            {
+                Console.WriteLine("Sorry! You did not passed the course. Good luck for next time.");
+            }
+        
     }
 }
