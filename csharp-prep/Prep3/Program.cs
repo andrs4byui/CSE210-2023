9c11fc138601c9c78ab3752925b92e6f76316725
Andres Soruco <mam20002@byui.edu>
2023-01-11 19:27:16 -0400

    Preparation 3 first step code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        String userNumberToGuess = Console.ReadLine();
+        Console.Write("What is your guess? ");
+        String userGuess = Console.ReadLine();
+
+        int theNumber = int.Parse(userNumberToGuess);
+        int theGuess = int.Parse(userGuess);
+
+        if (theNumber == theGuess)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        else if (theNumber > theGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+        else
+            {
+                Console.WriteLine("Lower");
+            }
+        ;   
     }
 }
