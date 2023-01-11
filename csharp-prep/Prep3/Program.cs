bf47c85b338bd0a9c1e3d223457c9a81d3c86621
Andres Soruco <mam20002@byui.edu>
2023-01-11 19:36:31 -0400

    Preparation 3 Second step code completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,26 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
+        int theNumber;
+        int theGuess;
+        do 
+        {
         Console.Write("What is the magic number? ");
         String userNumberToGuess = Console.ReadLine();
         Console.Write("What is your guess? ");
         String userGuess = Console.ReadLine();
 
-        int theNumber = int.Parse(userNumberToGuess);
-        int theGuess = int.Parse(userGuess);
+        theNumber = int.Parse(userNumberToGuess);
+        theGuess = int.Parse(userGuess);
 
-        if (theNumber == theGuess)
-            {
-                Console.WriteLine("You guessed it!");
-            }
-        else if (theNumber > theGuess)
+        if (theNumber > theGuess)
             {
                 Console.WriteLine("Higher");
             }
-        else
+        else if (theNumber < theGuess)
             {
                 Console.WriteLine("Lower");
             }
-        ;   
+        else 
+            {
+                Console.WriteLine("You guessed it");
+            }
+        } while (theGuess != theNumber);
     }
 }
