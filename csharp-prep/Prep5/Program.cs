a17e2c85ff986a12e8d91c8fd90bf5368009b197
Andres Soruco <mam20002@byui.edu>
2023-01-12 13:25:05 -0400

    Trying out prepare 5. c# code

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+
+        static void DisplayWelcome()
+            {
+                Console.WriteLine("Welcome to the program!");
+            };
+
+        static void PromptUserName(String userName)
+            {
+                Console.Write("Please enter your name: ");
+                userName = Console.ReadLine();
+                return;
+            };
+        static void PropmtUserNumber(int favoriteNumber)
+            {
+                Console.Write("Please enter you favorite number: ");
+                favoriteNumber = Console.Read();
+            };
+        static void SquareNumber();
+            {
+                Console.Write($"{userName} ");
+            }
+
+
     }
 }
