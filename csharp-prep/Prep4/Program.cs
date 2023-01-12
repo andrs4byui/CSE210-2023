3ee7eab39538ba806958a5127d06e0ebaef1c157
Andres Soruco <mam20002@byui.edu>
2023-01-12 12:29:16 -0400

    Core code completed. Working in Strech challenges

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,4 +1,5 @@
 using System;
+// This libraries are needed to call the methods of Sum, Average
 using System.Linq;
 using System.Collections.Generic;
 
@@ -19,9 +20,16 @@ class Program
             if (theNumberInput == 0)
                 {
                     int sumOfList = numbers.AsQueryable().Sum();
+                    double listAverage = Queryable.Average(numbers.AsQueryable());
+                    int maxValue = numbers.Max();
                     Console.WriteLine($"the sum is: {sumOfList}");
-                    //Console.WriteLine($"The average is: ${}");
-                    //Console.WriteLine($"The largest number is: ${}");
+                    Console.WriteLine($"The average is: {listAverage}");
+                    Console.WriteLine($"The largest number is: {maxValue}");
+
+                    //Strech Chanllenge
+                    int minValue = numbers.Min();
+                    Console.WriteLine($"Smallest positive number is: {}");
+                    //Console.WriteLine($"the sorted list is: {}");
                 }
             else 
                 {
