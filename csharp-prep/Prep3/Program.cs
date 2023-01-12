12aed2d4586d042870b10152bb67c975bf275de7
Andres Soruco <mam20002@byui.edu>
2023-01-12 12:17:17 -0400

    Preparation 4, Core requirement sum the list is completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -10,37 +10,56 @@ class Program
         //strech challenge Variables
         int numberOfGuess = 1;
 
-        Random randomGenerator = new Random();
-        theNumber = randomGenerator.Next(1,100);
         
-        
+        Boolean isPlaying = true;
+        String playerWantsToPlay = "";
 
-        do 
-        {
-        
-        Console.WriteLine("Guess the magic number!!");
-
-        Console.Write("What is your guess? ");
-        String userGuess = Console.ReadLine();
-
-        theGuess = int.Parse(userGuess);
-
-        if (theNumber > theGuess)
+        while (isPlaying == true) 
             {
-                Console.WriteLine("Higher");
-            }
-        else if (theNumber < theGuess)
-            {
-                Console.WriteLine("Lower");
-            }
-        else 
-            {
-                Console.WriteLine("You guessed it");
-                Console.WriteLine($"Your intents to guess were {numberOfGuess}");
-            };
+                Random randomGenerator = new Random();
+                theNumber = randomGenerator.Next(1,100);
 
-        numberOfGuess += 1;
-        } while (theGuess != theNumber);
+                Console.WriteLine("Guess the magic number!!");
+            do 
+            {
+
+            Console.Write("What is your guess? ");
+            String userGuess = Console.ReadLine();
+
+            theGuess = int.Parse(userGuess);
+
+            if (theNumber > theGuess)
+                {
+                    Console.WriteLine("Higher");
+                }
+            else if (theNumber < theGuess)
+                {
+                    Console.WriteLine("Lower");
+                }
+            else 
+                {
+                    Console.WriteLine("You guessed it");
+                    Console.WriteLine($"Your intents to guess were {numberOfGuess}");
+
+                };
+
+            numberOfGuess += 1;
+
+            } while (theGuess != theNumber);
+            
+            // Strech challenge
+            Console.Write("Do you want to play again? ");
+            playerWantsToPlay = Console.ReadLine();
+            if (playerWantsToPlay == "Yes")
+                {
+                    isPlaying = true;
+                }
+            else 
+                {
+                    isPlaying = false;
+                    Console.WriteLine("Thank you for playing");
+                }
         
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        int theNumberInput;
+        
+        do
+        {
+            Console.Write("Enter number: ");
+            String input = Console.ReadLine();
+            theNumberInput = int.Parse(input);
+            if (theNumberInput == 0)
+                {
+                    int sumOfList = numbers.AsQueryable().Sum();
+                    Console.WriteLine($"the sum is: {sumOfList}");
+                    //Console.WriteLine($"The average is: ${}");
+                    //Console.WriteLine($"The largest number is: ${}");
+                }
+            else 
+                {
+                    numbers.Add(theNumberInput);
+
+                }
+        } while (theNumberInput != 0);
     }
 }
