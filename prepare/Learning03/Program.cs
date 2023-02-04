e9a8bba659b65836ca37d54ac23673349bdb69f6
Andres Soruco <mam20002@byui.edu>
2023-02-04 11:20:56 -0400

    Steps were followed, code working properly

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        //Code Testing
+        Console.WriteLine(""); 
+        Console.WriteLine(""); 
+        Console.WriteLine("Here are the inputs to test the code");
+        Fraction try1 = new Fraction(1);
+        Fraction try2 = new Fraction(5);
+        Fraction try3 = new Fraction(3, 4);
+        Fraction Try4 = new Fraction(1, 3);
+
+
     }
 }
